12ea835d08d129de561566b9eef617535da87280
flafkus <122836545+flafkus@users.noreply.github.com>
2026-04-06 23:14:06 +0100

    add title screen + some colour to this

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from puzzle import Puzzle
 import json
+from colorama import Fore, Style, init
+
+init()
+
+def title_screen():
+    screen = f"""
+    {Fore.LIGHTBLACK_EX}-----------------------------
+    {Fore.LIGHTGREEN_EX}Computer Science Escape Room 
+    {Fore.LIGHTBLACK_EX}-----------------------------
+    
+    {Fore.LIGHTYELLOW_EX}  Press enter to begin...{Style.RESET_ALL}"""
+    
+    input(screen)
+
 
 with open('puzzles.json', 'r') as f:
     puzzles_json = json.load(f)
@@ -7,3 +21,6 @@ with open('puzzles.json', 'r') as f:
 
 p = Puzzle(puzzles[0])
 p.print_puzzle_info()
+
+    
+title_screen()
--- puzzle.py
+++ puzzle.py
@@ -5,9 +5,16 @@ class Puzzle():
         self.difficulty = config['difficulty']
         self.data = config['data']
         self.hints = config['hints']
+        self.solved = False
     
     def print_puzzle_info(self):
         print(f'title: {self.title}')
         print(f'difficulty: {self.difficulty}')
         print(f'data: {self.data}')
         print(f'hints: {self.hints}')
+        print(f'solved: {self.solved}')
+
+    # todo -->  make generic functions to interact with puzzles
+    #           make function to display the puzzle
+    #           make function to give hint to person
+    
